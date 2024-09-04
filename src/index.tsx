ce96df98abb59959fea0d44baeb333e336e77a91
joy859 <nangajoy647@gmail.com>
2024-09-04 15:30:51 +0100

    package

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,23 @@
 import ReactDom  from 'react-dom/client'
-import App from './App'
-
+import App from './App';
+import{Provider} from "react-redux";
+import {store} from'./Redux/store';
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from "react-toastify"
 import"./Assets/global.css";
-ReactDom.createRoot(document.getElementById('root') as HTMLElement).render(<App/>)
+
+ReactDom.createRoot(document.getElementById('root') as HTMLElement).render(
+<Provider store={store}>
+    <App/>
+    <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnHover
+        theme="light" 
+        />
+    </Provider>
+    );
